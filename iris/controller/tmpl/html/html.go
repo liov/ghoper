e026bcbf469048b41328528768c2616fd4779ca0
贾一饼 <lby.i@qq.com>
2019-02-12 14:36:02 +0800

    日志文件

--- iris/controller/tmpl/html/html.go
+++ iris/controller/tmpl/html/html.go
@@ -3,6 +3,7 @@ package html
 import "github.com/kataras/iris"
 
 func HtmlTest(ctx iris.Context) {
+	ctx.Application().Logger().Infof("Request path: %s", ctx.Path())
 	ctx.ViewData("Title", "Hi Page")
 	ctx.ViewData("Name", "iris")
 	ctx.View("hi.html")
