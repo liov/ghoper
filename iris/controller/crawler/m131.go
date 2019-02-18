3a5825b93362589de1c06212027e567289334c8f
贾一饼 <lby.i@qq.com>
2019-02-18 11:36:07 +0800

    goquery 爬虫

--- iris/controller/crawler/m131.go
+++ iris/controller/crawler/m131.go
@@ -17,35 +17,40 @@ func M131() {
 
 	doc := getDoc("http://www.mm131.com/xinggan/", "text/html; charset=gb2312")
 	max := 0
-	link := doc.Find("a[target=_blank]").Nodes[0].Attr[1].Val
-	pattern := regexp.MustCompile(`\d{4,}`).FindAllString(link, -1)
-	if len(pattern) > 0 {
-		max, _ = strconv.Atoi(pattern[0])
+	if doc != nil {
+		link := doc.Find("a[target=_blank]").Nodes[0].Attr[1].Val
+		pattern := regexp.MustCompile(`\d{4,}`).FindAllString(link, -1)
+		if len(pattern) > 0 {
+			max, _ = strconv.Atoi(pattern[0])
+		}
 	}
 
 	for max != 0 {
 		page := strconv.Itoa(max)
 		url := "http://www.mm131.com/xinggan/" + page + ".html"
 		doc := getDoc(url, "text/html; charset=gb2312")
-		s1 := doc.Find("h5").Text()
-		dir := "E:/pic/" + s1
-		s := doc.Find("span.page-ch").Nodes[0].FirstChild.Data
-		pattern := regexp.MustCompile(`\d+`).FindAllString(s, -1)
-		if len(pattern) > 0 {
-			dir = dir + pattern[0] + "P"
-			fmt.Println(dir)
-			if utils.CheckNotExist(dir) == true {
-				if err := utils.Mkdir(dir); err != nil {
-					fmt.Println(err)
+
+		if doc != nil {
+			s1 := doc.Find("h5").Text()
+			dir := "E:/pic/" + s1
+			s := doc.Find("span.page-ch").Nodes[0].FirstChild.Data
+			pattern := regexp.MustCompile(`\d+`).FindAllString(s, -1)
+			if len(pattern) > 0 {
+				dir = dir + pattern[0] + "P"
+				fmt.Println(dir)
+				if utils.CheckNotExist(dir) == true {
+					if err := utils.Mkdir(dir); err != nil {
+						fmt.Println(err)
+					}
+				} else {
+					return
 				}
-			} else {
-				return
+				num, _ := strconv.Atoi(pattern[0])
+				for i := 1; i <= num; i++ {
+					getImg(strconv.Itoa(max), strconv.Itoa(i), dir)
+				}
+				max--
 			}
-			num, _ := strconv.Atoi(pattern[0])
-			for i := 1; i <= num; i++ {
-				getImg(strconv.Itoa(max), strconv.Itoa(i), dir)
-			}
-			max--
 		}
 	}
 
@@ -59,7 +64,7 @@ func getDoc(url string, contentType string) *goquery.Document {
 	}
 	defer res.Body.Close()
 	if res.StatusCode != 200 {
-		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+		return nil
 	}
 
 	rb, _ := ioutil.ReadAll(res.Body)
