680c7e46ffe431076bc5bbf4250598c155891361
贾一饼 <lby.i@qq.com>
2019-02-26 12:14:57 +0800

    header

--- website/antd/layouts/default.vue
+++ website/antd/layouts/default.vue
@@ -1,9 +1,68 @@
 <template>
   <a-locale-provider :locale="zh_CN">
     <a-layout>
-      <a-layout-header style="text-align: center">
-        header
-      </a-layout-header>
+      <div class="header" style="background-color: #fff">
+        <div class="menu">
+          <a-menu
+            v-model="current"
+            :theme="theme"
+            mode="horizontal"
+            :style="{ lineHeight: '64px' }"
+          >
+            <a-menu-item key="home">
+              <nuxt-link to="/">
+                <a-icon type="home" /> 主页
+              </nuxt-link>
+            </a-menu-item>
+            <a-menu-item key="app" disabled>
+              <a-icon type="appstore" />Hoper
+            </a-menu-item>
+            <a-menu-item key="file-text">
+              <nuxt-link to="/article">
+                <a-icon type="file-text" /> 博客
+              </nuxt-link>
+            </a-menu-item>
+            <a-menu-item key="book">
+              <nuxt-link to="/diary">
+                <a-icon type="book" /> 日记
+              </nuxt-link>
+            </a-menu-item>
+            <a-menu-item key="picture">
+              <nuxt-link to="/moment">
+                <a-icon type="picture" /> 瞬间
+              </nuxt-link>
+            </a-menu-item>
+            <a-menu-item key="chat">
+              <nuxt-link to="/chat">
+                <a-icon type="message" />聊天
+              </nuxt-link>
+            </a-menu-item>
+            <a-sub-menu>
+              <span slot="title" class="submenu-title-wrapper"><a-icon type="setting" />设置</span>
+              <a-menu-item-group title="Item 1">
+                <a-menu-item key="setting:1">
+                  Option 1
+                </a-menu-item>
+                <a-menu-item key="setting:2">
+                  Option 2
+                </a-menu-item>
+              </a-menu-item-group>
+              <a-menu-item-group title="Item 2">
+                <a-menu-item key="setting:3">
+                  Option 3
+                </a-menu-item>
+                <a-menu-item key="setting:4">
+                  Option 4
+                </a-menu-item>
+              </a-menu-item-group>
+            </a-sub-menu>
+            <a-switch
+              :default-checked="false"
+              @change="changeTheme"
+            /> 主题
+          </a-menu>
+        </div>
+      </div>
       <a-layout-content>
         <nuxt />
       </a-layout-content>
@@ -17,13 +76,37 @@
 import zh_CN from 'ant-design-vue/lib/locale-provider/zh_CN'
 import moment from 'moment'
 import 'moment/locale/zh-cn'
+import ALayoutSider from 'ant-design-vue/es/layout/Sider'
 
 moment.locale('zh-cn')
 export default {
+  components: { ALayoutSider },
   data() {
     return {
+      current: ['main'],
+      theme: 'light',
+      style: { backgroundColor: '#fff' },
       zh_CN
+    }
+  },
+  methods: {
+    changeTheme(checked) {
+      this.theme = checked ? 'dark' : 'light'
+      this.style = checked
+        ? { height: '48px' }
+        : { backgroundColor: '#fff', height: '48px' }
     }
   }
 }
 </script>
+<style scoped>
+.header {
+  height: 64px;
+}
+.menu {
+  width: 100%;
+  text-align: center;
+  position: fixed;
+  z-index: 1;
+}
+</style>
--- website/antd/pages/index.vue
+++ website/antd/pages/index.vue
@@ -3,7 +3,7 @@
     <div>
       <logo />
       <h1 class="title">
-        antd
+        hoper
       </h1>
       <h2 class="subtitle">
         hoper
