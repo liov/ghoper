1355aec9ddfa5f9b72ffe91d52e6a21107359042
贾一饼 <lby.i@qq.com>
2019-03-08 18:01:59 +0800

    手写轮子，字段赋值

--- micro/utils/copy.go
+++ micro/utils/copy.go
@@ -5,12 +5,12 @@ import (
 	"reflect"
 )
 
-func Copy(source interface{}, tatget interface{}) error {
+func Copy(source interface{}, target interface{}) error {
 	if Stype := reflect.TypeOf(source).Kind(); Stype != reflect.Struct || Stype != reflect.Ptr {
 		return errors.New("source is not a struct")
 	}
-	if Ttype := reflect.TypeOf(tatget).Kind(); Ttype != reflect.Struct || Ttype != reflect.Ptr {
-		return errors.New("source is not a struct")
+	if Ttype := reflect.TypeOf(target).Kind(); Ttype != reflect.Struct || Ttype != reflect.Ptr {
+		return errors.New("target is not a struct")
 	}
 	return nil
 }
