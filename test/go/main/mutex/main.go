9beee99ed29fac5bbcf379a8b3d2e9f92485629a
贾一饼 <lby.i@qq.com>
2019-03-22 21:01:03 +0800

    mutex

--- test/go/main/mutex/main.go
+++ test/go/main/mutex/main.go
@@ -6,6 +6,24 @@ import (
 	"time"
 )
 
+type Book struct {
+	BookName string
+	L        *sync.Mutex
+}
+
+func (bk *Book) SetName(wg *sync.WaitGroup, name string) {
+	defer func() {
+		fmt.Println("Unlock set name:", name)
+		bk.L.Unlock()
+		wg.Done()
+	}()
+
+	bk.L.Lock()
+	fmt.Println("Lock set name:", name)
+	time.Sleep(1 * time.Second)
+	bk.BookName = name
+}
+
 func main() {
 	c := sync.NewCond(&sync.Mutex{})    //1
 	queue := make([]interface{}, 0, 10) //2
@@ -55,6 +73,19 @@ func main() {
 	fmt.Println("ready unlock (G0)")
 	mutex.Unlock()
 	fmt.Println("unlocked (G0)")
+	wg.Wait()
+}
+
+func mutexStruct() {
+
+	bk := Book{}
+	bk.L = new(sync.Mutex)
+	wg := &sync.WaitGroup{}
+	books := []string{"《三国演义》", "《道德经》", "《西游记》"}
+	for _, book := range books {
+		wg.Add(1)
+		go bk.SetName(wg, book)
+	}
 
 	wg.Wait()
 }
