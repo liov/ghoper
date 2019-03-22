badfdd8b27a43a78b8405969a164436686cf4426
贾一饼 <lby.i@qq.com>
2019-03-22 20:58:57 +0800

    mutex

--- test/go/main/mutex/main.go
+++ test/go/main/mutex/main.go
@@ -29,4 +29,32 @@ func main() {
 		go removeFromQueue(1 * time.Second) //6
 		c.L.Unlock()                        //7
 	}
+
+	wg := sync.WaitGroup{}
+
+	var mutex sync.Mutex
+	fmt.Println("Locking  (G0)")
+	mutex.Lock()
+	fmt.Println("locked (G0)")
+	wg.Add(3)
+
+	for i := 1; i < 4; i++ {
+		go func(i int) {
+			fmt.Printf("Locking (G%d)\n", i)
+			mutex.Lock()
+			fmt.Printf("locked (G%d)\n", i)
+
+			time.Sleep(time.Second * 2)
+			mutex.Unlock()
+			fmt.Printf("unlocked (G%d)\n", i)
+			wg.Done()
+		}(i)
+	}
+
+	time.Sleep(time.Second * 5)
+	fmt.Println("ready unlock (G0)")
+	mutex.Unlock()
+	fmt.Println("unlocked (G0)")
+
+	wg.Wait()
 }
