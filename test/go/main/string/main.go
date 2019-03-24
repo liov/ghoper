5c59aff2021a99cbcdfc852544a783c2e6525708
actliboy <lby.i@qq.com>
2019-03-24 17:03:42 +0800

    push.js,突然觉的c/cpp还是有必要学一下的，至少得能看懂代码，当然，得在啃完rust之后

--- test/go/main/string/main.go
+++ test/go/main/string/main.go
@@ -1,8 +1,20 @@
 package main
 
+import (
+	"bytes"
+	"fmt"
+)
+
 func main() {
 	var s string
 	if s == "" {
 		print("初始化为空")
 	}
+
+	var buffer bytes.Buffer
+	buffer.WriteString("hello")
+	buffer.WriteString(", ")
+	buffer.WriteString("world")
+
+	fmt.Print(buffer.String())
 }
---
+++ website/vhoper/pages/other/push.vue
@@ -0,0 +1,29 @@
+<template>
+  <div />
+</template>
+
+<script>
+import Push from 'push.js'
+
+export default {
+  mounted() {
+    const promise = Push.create('Hello world!', {
+      body: "How's it hangin'?",
+      icon: '/icon.png',
+      timeout: 4000,
+      onClick: function() {
+        window.focus()
+        this.close()
+      }
+    })
+    promise.then(function(notification) {
+      notification.close()
+    })
+    Push.clear()
+  },
+  methods: {}
+}
+</script>
+
+<style scoped>
+</style>
