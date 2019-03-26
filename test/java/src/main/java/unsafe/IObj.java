def742191338d4872ec10fbe7de4d6a9838c75b3
贾一饼 <lby.i@qq.com>
2019-03-26 17:09:54 +0800

    unsafe

--- test/java/src/main/java/unsafe/IObj.java
+++ test/java/src/main/java/unsafe/IObj.java
@@ -24,8 +24,10 @@ public class IObj {
         Field field = IObj.class.getDeclaredField("objField");
         long offset = U.objectFieldOffset(field);
         IObj obj = new IObj();
-
         int val = U.getInt(obj, offset);
         System.out.println("1.\t" + val + "\t" + (val == 10));
+        U.putInt(obj, offset,11);
+        System.out.println(U.getAddress(obj,offset));
+        System.out.println("2.\t" + U.getInt(obj, offset) + "\t" + (U.getInt(obj, offset) == 10));
     }
 }
