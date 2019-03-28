26ade6db7836734be3ad86d4751e9d69a17916f9
贾一饼 <lby.i@qq.com>
2019-03-28 12:28:19 +0800

    结构整改

--- micro/model/e/err.go
+++ micro/model/e/err.go
@@ -7,19 +7,3 @@ type ValidtionError struct {
 func (e *ValidtionError) Error() string {
 	return e.Msg
 }
-
-type DBError struct {
-	Msg string
-}
-
-func (e *DBError) Error() string {
-	return e.Msg
-}
-
-type LimitError struct {
-	Msg string
-}
-
-func (e *LimitError) Error() string {
-	return e.Msg
-}
