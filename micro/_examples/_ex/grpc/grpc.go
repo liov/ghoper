ef273c730c2ceda74be2f77aa406c46c4cfe8542
贾一饼 <lby.i@qq.com>
2019-04-04 16:02:39 +0800

    改依赖

--- micro/_examples/_ex/grpc/grpc.go
+++ micro/_examples/_ex/grpc/grpc.go
@@ -1,10 +1,9 @@
 package main
 
 import (
-	"log"
-
 	"github.com/golang/protobuf/proto"
-	"test/protobuf/example"
+	"hoper/_examples/_ex/grpc/example"
+	"log"
 )
 
 func main() {
--- test/go/go.mod
+++
@@ -1,5 +0,0 @@
-module test
-
-go 1.12
-
-require github.com/liov/ghoper v0.0.0-20190404020342-30c17c5d63f3 // indirect
