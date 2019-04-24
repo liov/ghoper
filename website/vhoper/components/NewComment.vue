73bad9272a159561e4baaa92241b6e63e535297d
贾一饼 <lby.i@qq.com>
2019-04-24 20:14:30 +0800

    comment

--- website/vhoper/components/NewComment.vue
+++ website/vhoper/components/NewComment.vue
@@ -67,67 +67,14 @@
         >
           <a-collapse-panel key="1" header="收起评论">
             <div class="sub-comments">
-              <a-list
-                item-layout="horizontal"
-                :data-source="item.sub_comments"
-              >
-                <div slot="loadMore" :style="{ textAlign: 'center', marginTop: '12px', height: '32px', lineHeight: '32px' }">
-                  <a-spin v-if="loading" />
-                  <span v-else>已经到底</span>
-                </div>
-                <a-list-item slot="renderItem" slot-scope="subComment,subIndex">
-                  <a-comment
-                    :key="subIndex"
-                    :author="subComment.user.name"
-                    :avatar="subComment.user.avatar_url"
-                  >
-                    <span slot="actions">
-                      <span>
-                        <a-tooltip title="Like">
-                          <a-icon
-                            type="like"
-                            :theme="action === 'liked' ? 'filled' : 'outlined'"
-                            @click="like"
-                          />
-                        </a-tooltip>
-                        <span style="padding:0 8px;cursor: auto">
-                          {{ likes }}
-                        </span>
-                      </span>
-                      <span>
-                        <a-tooltip title="Dislike">
-                          <a-icon
-                            type="dislike"
-                            :theme="action === 'disliked' ? 'filled' : 'outlined'"
-                            @click="dislike"
-                          />
-                        </a-tooltip>
-                        <span style="padding:0 8px;cursor: auto">
-                          {{ dislikes }}
-                        </span>
-                      </span>
-                      <span style="padding-right: 8px" @click="$emit('reply',subComment)">回复</span>
-                      <span v-if="$store.state.user&&$store.state.user.id === subComment.user.id " style="padding:0 8px" @click="delComment(subComment.id)">删除</span>
-                    </span>
-                    <template slot="content">
-                      <div>
-                        {{ subComment.content }}
-                      </div>
-                    </template>
-                    <span v-if="subComment.parent_id !== subComment.root_id" slot="datetime" :title="subComment.user.name">
-                      <span>@<nuxt-link :to="'/user/'+subComment.recv_user.id">{{ subComment.user.name }}</nuxt-link></span>
-                      <a-divider type="vertical" />
-                    </span>
-                    <a-tooltip slot="datetime" :title="subComment.created_at|dateFormat">
-                      <span>{{ subComment.created_at|dateFormat }}</span>
-                      <a-divider type="vertical" />
-                    </a-tooltip>
-                    <a-tooltip slot="datetime">
-                      <span>{{ $s2date(subComment.created_at).fromNow() }}</span>
-                    </a-tooltip>
-                  </a-comment>
-                </a-list-item>
-              </a-list>
+              <sub-comment
+                :sub-comments="item.sub_comments"
+                :index="index"
+                @more="onLoadMore"
+                @like="like"
+                @dislike="dislike"
+                @delComment="delComment"
+              />
             </div>
           </a-collapse-panel>
         </a-collapse>
@@ -139,8 +86,10 @@
 <script>
 // $emit必须重启 npm run
 
+import SubComment from './SubComment'
 export default {
   name: 'NewComment',
+  components: { SubComment },
   // directives: { infiniteScroll },
   // components: { 'virtual-scroller': VirtualScroller },
   props: ['kind', 'count'],
@@ -170,43 +119,25 @@ export default {
         // simulate ajax call to add content using the function below
         vm.onLoadMore()
       })
-
-    this.$nextTick(() => {
-      const els = document.querySelectorAll('.sub-comments')
-      els.forEach(el => {
-        el.addEventListener(
-          'scroll',
-          () => {
-            const offsetHeight = el.offsetHeight
-            const scrollTop = el.scrollTop
-            const scrollHeight = el.scrollHeight
-            if (offsetHeight + scrollTop - scrollHeight >= -1) {
-              console.log(3)
-            }
-            console.log(2)
-          },
-          true
-        )
-      })
-    })
   },
   methods: {
-    like() {
+    like(id) {
       this.likes += 1
       this.action = 'liked'
+      console.log(id)
     },
-    dislike() {
+    dislike(id) {
       this.dislikes += 1
       this.action = 'disliked'
+      console.log(id)
     },
 
     delComment(id) {
       console.log(id)
     },
-    async onLoadMore(e, index) {
+    async onLoadMore(index) {
       console.log(1)
       if (index) {
-        console.log(e.target)
         if (
           this.comments[index].sub_comments !== null &&
           this.comments[index].sub_comments.length ===
---
+++ website/vhoper/components/SubComment.vue
@@ -0,0 +1,99 @@
+<template>
+  <a-list
+    item-layout="horizontal"
+    :data-source="subComments"
+  >
+    <div slot="loadMore" :style="{ textAlign: 'center', marginTop: '12px', height: '32px', lineHeight: '32px' }">
+      <a-spin v-if="loading" :id="'loading'+index" />
+      <span v-else>已经到底</span>
+    </div>
+    <a-list-item slot="renderItem" slot-scope="subComment,subIndex">
+      <a-comment
+        :key="subIndex"
+        :author="subComment.user.name"
+        :avatar="subComment.user.avatar_url"
+      >
+        <span slot="actions">
+          <span>
+            <a-tooltip title="Like">
+              <a-icon
+                type="like"
+                :theme="action === 'liked' ? 'filled' : 'outlined'"
+                @click="$emit('like',subComment.id)"
+              />
+            </a-tooltip>
+            <span style="padding:0 8px;cursor: auto">
+              {{ likes }}
+            </span>
+          </span>
+          <span>
+            <a-tooltip title="Dislike">
+              <a-icon
+                type="dislike"
+                :theme="action === 'disliked' ? 'filled' : 'outlined'"
+                @click="$emit('dislike',subComment.id)"
+              />
+            </a-tooltip>
+            <span style="padding:0 8px;cursor: auto">
+              {{ dislikes }}
+            </span>
+          </span>
+          <span style="padding-right: 8px" @click="$emit('reply',subComment)">回复</span>
+          <span v-if="$store.state.user&&$store.state.user.id === subComment.user.id " style="padding:0 8px" @click="$emit('delComment',subComment.id)">删除</span>
+        </span>
+        <template slot="content">
+          <div>
+            {{ subComment.content }}
+          </div>
+        </template>
+        <span v-if="subComment.parent_id !== subComment.root_id" slot="datetime" :title="subComment.user.name">
+          <span>@<nuxt-link :to="'/user/'+subComment.recv_user.id">{{ subComment.user.name }}</nuxt-link></span>
+          <a-divider type="vertical" />
+        </span>
+        <a-tooltip slot="datetime" :title="subComment.created_at|dateFormat">
+          <span>{{ subComment.created_at|dateFormat }}</span>
+          <a-divider type="vertical" />
+        </a-tooltip>
+        <a-tooltip slot="datetime">
+          <span>{{ $s2date(subComment.created_at).fromNow() }}</span>
+        </a-tooltip>
+      </a-comment>
+    </a-list-item>
+  </a-list>
+</template>
+
+<script>
+export default {
+  name: 'SubComment',
+  props: ['subComments', 'index'],
+  data() {
+    return {
+      likes: 0,
+      dislikes: 0,
+      action: null,
+      loading: true,
+      showLoading: true
+    }
+  },
+  mounted() {
+    const vm = this
+
+    const ScrollMagic = require('scrollmagic')
+
+    const controller = new ScrollMagic.Controller()
+    // build scene
+    const scene = new ScrollMagic.Scene({
+      triggerElement: '#loading' + vm.$props.index,
+      triggerHook: 'onEnter'
+    })
+      .addTo(controller)
+      .on('enter', function(e) {
+        // simulate ajax call to add content using the function below
+        vm.$emit('more', vm.$props.index)
+      })
+  }
+}
+</script>
+
+<style scoped>
+</style>
