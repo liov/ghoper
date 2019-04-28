dcf4aace9ff76288d388605620e85b11b3dca85d
贾一饼 <lby.i@qq.com>
2019-04-28 18:20:37 +0800

    跳转验证无cookie登录

--- website/vhoper/pages/user/redirect.vue
+++ website/vhoper/pages/user/redirect.vue
@@ -10,9 +10,8 @@ export default {
       this.$store.commit('SET_TOKEN', res.token)
       this.$axios.defaults.headers.common.Authorization = token
       this.$axios.$get(`/api/user/get`).then(res => {
-        if (res.code === 200) {
-          this.$store.commit('SET_USER', res.data)
-        } else this.$router.replace(this.$route.query.callbackUrl)
+        if (res.code === 200) this.$store.commit('SET_USER', res.data)
+        this.$router.replace(this.$route.query.callbackUrl)
       })
     }
   }
