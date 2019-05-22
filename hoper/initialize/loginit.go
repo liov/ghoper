baaa6cef136a956a38374b34ba1045acd177a5d1
贾一饼 <lby.i@qq.com>
2019-05-22 18:35:41 +0800

    log改造

--- hoper/initialize/loginit.go
+++ hoper/initialize/loginit.go
@@ -1,10 +1,29 @@
 package initialize
 
+import (
+	"fmt"
+	"github.com/kataras/golog"
+	"github.com/kataras/pio"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"runtime"
+)
+
 /**
  * @author     ：lbyi
  * @date       ：Created in 2019/4/2
  * @description：
  */
+
+type Logger interface {
+	Debug(args ...interface{})
+	Error(args ...interface{})
+	Fatal(args ...interface{})
+	Info(args ...interface{})
+	Warn(args ...interface{})
+}
+
+var Log Logger
 
 func initializeLog() {
 	// Log as JSON instead of the default ASCII formatter.
@@ -23,7 +42,7 @@ func initializeLog() {
 		logrus.SetFormatter(formatter)
 		// Only log the warning severity or above.
 		logrus.SetLevel(logrus.InfoLevel)*/
-/*	golog.SetTimeFormat("2006/01/02 15:04:05")
+	golog.SetTimeFormat("2006/01/02 15:04:05")
 	golog.Handle(func(l *golog.Log) bool {
 		//应该是深度，好烦，不带format是6，带是7，无法确定
 		pc, file, line, _ := runtime.Caller(6)
@@ -42,5 +61,27 @@ func initializeLog() {
 			l.Logger.Printer.Print(l)
 		}
 		return true
-	})*/
+	})
+	if Config.Server.Env == "debug"{
+		Log = golog.Default
+	}else {
+		config := zap.NewProductionConfig()
+		config.EncoderConfig = zapcore.EncoderConfig{
+			TimeKey:        "ts",
+			LevelKey:       "level",
+			NameKey:        "logger",
+			CallerKey:      "caller",
+			MessageKey:     "msg",
+			StacktraceKey:  "stacktrace",
+			LineEnding:     zapcore.DefaultLineEnding,
+			EncodeLevel:    zapcore.LowercaseLevelEncoder,
+			EncodeTime:     zapcore.ISO8601TimeEncoder,
+			EncodeDuration: zapcore.SecondsDurationEncoder,
+			EncodeCaller:   zapcore.ShortCallerEncoder,
+		}
+		logger, _ := config.Build()
+		defer logger.Sync()
+		Log = logger.Sugar()
+	}
+
 }
