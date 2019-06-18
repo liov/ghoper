9ed8ccfec87601ef109ac2f8d677382353bbd03e
贾一饼 <lby.i@qq.com>
2019-06-18 14:15:20 +0800

    go中的资源泄露

--- test/go/interview/defer.go
+++ test/go/interview/defer.go
@@ -310,7 +310,18 @@ func iota_() {
 	fmt.Println(d)
 }
 
-//为什么我不觉得存在资源泄露，资源泄露指的是什么
+//在搜索重复时依旧每次都起一个 goroutine 去处理，每个 goroutine 都把它的搜索结果发送到结果 channel 中，channel 中收到的第一条数据会直接返回。
+//
+//返回完第一条数据后，其他 goroutine 的搜索结果怎么处理？他们自己的协程如何处理？
+//
+//在 First() 中的结果 channel 是无缓冲的，这意味着只有第一个 goroutine 能返回，由于没有 receiver，其他的 goroutine 会在发送上一直阻塞。如果你大量调用，则可能造成资源泄露。
+//
+//为避免泄露，你应该确保所有的 goroutine 都能正确退出，有 2 个解决方法：
+//
+//使用带缓冲的 channel，确保能接收全部 goroutine 的返回结果：
+//使用 select 语句，配合能保存一个缓冲值的 channel default 语句：
+//default 的缓冲 channel 保证了即使结果 channel 收不到数据，也不会阻塞 goroutine
+//使用特殊的废弃（cancellation） channel 来中断剩余 goroutine 的执行：
 type query func(string) string
 
 func exec(name string, vs ...query) string {
