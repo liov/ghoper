53e850a60e1d91ef9414af372fb063208b86f263
贾一饼 <lby.i@qq.com>
2019-07-08 17:47:09 +0800

    rust多线程,通道，锁，wait/notify,原子值

--- test/rust/cmd/src/bin/thread.rs
+++ test/rust/cmd/src/bin/thread.rs
@@ -1,8 +1,9 @@
-use std::sync::{mpsc, Arc};
+use std::sync::{mpsc, Arc, Mutex, Condvar};
 use std::{thread, fmt};
 use std::rc::Rc;
 use std::fmt::Display;
 use std::time::Duration;
+use std::sync::atomic::{AtomicUsize,Ordering};
 
 pub struct Foo<T>{
     id:Rc<T>
@@ -20,8 +21,6 @@ unsafe impl<T> Send for Foo<T> {}
 const THREAD_COUNT :i32 = 10;
 
 fn main(){
-    let var : Arc<i32> = Arc::new(5);
-    let share_var = var.clone();
 
     // 创建一个通道
     let (tx, rx)= mpsc::sync_channel(0);
@@ -47,7 +46,48 @@ fn main(){
 
     new_thread.join().unwrap();
 
-    let new_thread = thread::spawn(move || {
+    let var : Arc<i32> = Arc::new(5);
+    let share_var = var.clone();
 
+    let new_thread = thread::spawn(move || {
+        println!("share value in new thread: {}, address: {:p}", share_var, &*share_var);
     });
+
+    new_thread.join().unwrap();
+    println!("share value in main thread: {}, address: {:p}", var, &*var);
+
+    let pair = Arc::new((Mutex::new(false), Condvar::new()));
+    let pair2 = pair.clone();
+
+    // 创建一个新线程
+    thread::spawn(move|| {
+        let &(ref lock, ref cvar) = &*pair2;
+        let mut started = lock.lock().unwrap();
+        *started = true;
+        cvar.notify_one();
+        println!("notify main thread");
+    });
+
+    // 等待新线程先运行
+    let &(ref lock, ref cvar) = &*pair;
+    let mut started = lock.lock().unwrap();
+    while !*started {
+        println!("before wait");
+        started = cvar.wait(started).unwrap();
+        println!("after wait");
+    }
+
+    let var : Arc<AtomicUsize> = Arc::new(AtomicUsize::new(5));
+    let share_var = var.clone();
+
+    // 创建一个新线程
+    let new_thread = thread::spawn(move|| {
+        println!("share value in new thread: {}", share_var.load(Ordering::SeqCst));
+        // 修改值
+        share_var.store(9, Ordering::SeqCst);
+    });
+
+    // 等待新建线程先执行
+    new_thread.join().unwrap();
+    println!("share value in main thread: {}", var.load(Ordering::SeqCst));
 }
