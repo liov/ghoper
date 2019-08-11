746f4de4de56e8c0637c9037bf4d5008ed41f068
liov <lby.i@qq.com>
2019-08-11 19:43:08 +0800

    julia

--- test/besides/Julia.jl/src/foo.jl
+++ test/besides/Julia.jl/src/foo.jl
@@ -5,4 +5,15 @@ foo:
 - Date: 2019-08-10
 =#
 using Gadfly
-plot(y=[1,2,3])
+set_default_plot_size(8inch, 3inch)
+
+dodge1 = plot(x=["a","a"], y=[3,2], color=["red","blue"],
+      Geom.bar(position=:dodge));
+dodge2 = plot(x=["a","a","b","b"], y=[3,2,1,2], color=["red","blue","red","blue"],
+      Geom.bar(position=:dodge));
+stack1 = plot(x=["a","a"], y=[3,2], color=["red","blue"],
+      Geom.bar(position=:stack));
+stack2 = plot(x=["a","a","b","b"], y=[3,2,1,2], color=["red","blue","red","blue"],
+      Geom.bar(position=:stack));
+
+gridstack([dodge1 dodge2; stack1 stack2])
