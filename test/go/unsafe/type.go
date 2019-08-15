d12d0ab852583995b1eb15fcf66d89b2c46b48f1
贾一饼 <lby.i@qq.com>
2019-08-15 17:52:42 +0800

    自制泛型失败

--- test/go/unsafe/type.go
+++ test/go/unsafe/type.go
@@ -3,147 +3,20 @@ package main
 import (
 	"fmt"
 	"reflect"
+	_type "test/unsafe/type"
 	"unsafe"
 )
-
-type rtype struct {
-	size       uintptr
-	ptrdata    uintptr  // number of bytes in the type that can contain pointers
-	hash       uint32   // hash of type; avoids computation in hash tables
-	tflag      tflag    // extra type information flags
-	align      uint8    // alignment of variable with this type
-	fieldAlign uint8    // alignment of struct field with this type
-	kind       uint8    // enumeration for C
-	alg        *typeAlg // algorithm table
-	gcdata     *byte    // garbage collection data
-	str        nameOff  // string form
-	ptrToThis  typeOff  // type for pointer to this type, may be zero
-}
-
-type tflag uint8
-type nameOff int32 // offset to a name
-type typeOff int32 // offset to an *rtype
-type textOff int32 // offset from top of text section
-// a copy of runtime.typeAlg
-type typeAlg struct {
-	// function for hashing objects of this type
-	// (ptr to object, seed) -> hash
-	hash func(unsafe.Pointer, uintptr) uintptr
-	// function for comparing objects of this type
-	// (ptr to object A, ptr to object B) -> ==?
-	equal func(unsafe.Pointer, unsafe.Pointer) bool
-}
-
-type method struct {
-	name nameOff // name of method
-	mtyp typeOff // method type (without receiver)
-	ifn  textOff // fn used in interface call (one-word receiver)
-	tfn  textOff // fn used for normal method call
-}
-
-// uncommonType is present only for defined types or types with methods
-// (if T is a defined type, the uncommonTypes for T and *T have methods).
-// Using a pointer to this struct reduces the overall size required
-// to describe a non-defined type with no methods.
-type uncommonType struct {
-	pkgPath nameOff // import path; empty for built-in types like int, string
-	mcount  uint16  // number of methods
-	xcount  uint16  // number of exported methods
-	moff    uint32  // offset from this uncommontype to [mcount]method
-	_       uint32  // unused
-}
-
-// ChanDir represents a channel type's direction.
-type ChanDir int
-
-type arrayType struct {
-	rtype
-	elem  *rtype // array element type
-	slice *rtype // slice type
-	len   uintptr
-}
-
-// chanType represents a channel type.
-type chanType struct {
-	rtype
-	elem *rtype  // channel element type
-	dir  uintptr // channel direction (ChanDir)
-}
-
-type funcType struct {
-	rtype
-	inCount  uint16
-	outCount uint16 // top bit is set if last input parameter is ...
-}
-
-// imethod represents a method on an interface type
-type imethod struct {
-	name nameOff // name of method
-	typ  typeOff // .(*FuncType) underneath
-}
-
-// interfaceType represents an interface type.
-type interfaceType struct {
-	rtype
-	pkgPath name      // import path
-	methods []imethod // sorted by hash
-}
-
-// mapType represents a map type.
-type mapType struct {
-	rtype
-	key        *rtype // map key type
-	elem       *rtype // map element (value) type
-	bucket     *rtype // internal bucket structure
-	keysize    uint8  // size of key slot
-	valuesize  uint8  // size of value slot
-	bucketsize uint16 // size of bucket
-	flags      uint32
-}
-
-// ptrType represents a pointer type.
-type ptrType struct {
-	rtype
-	elem *rtype // pointer element (pointed at) type
-}
-
-// sliceType represents a slice type.
-type sliceType struct {
-	rtype
-	elem *rtype // slice element type
-}
-
-// Struct field
-type structField struct {
-	name        name    // name is always non-empty
-	typ         *rtype  // type of field
-	offsetEmbed uintptr // byte offset of field<<1 | isEmbedded
-}
-
-type structType struct {
-	rtype
-	pkgPath name
-	fields  []structField // sorted by offset
-}
-
-type name struct {
-	bytes *byte
-}
-
-type emptyInterface struct {
-	typ  *rtype
-	word unsafe.Pointer
-}
 
 type Foo struct {}
 
 func main() {
 	var f interface{}
 	f = 1
-	b:= (*emptyInterface)(unsafe.Pointer(&f))
-	fmt.Println(b.typ)
-	fmt.Println(reflect.Kind(b.typ.kind&((1 << 5) - 1)))
+	b:= (*_type.EmptyInterface)(unsafe.Pointer(&f))
+	fmt.Println(b.Typ)
+	fmt.Println(reflect.Kind(b.Typ.Kind&((1 << 5) - 1)))
 	f = Foo{}
-	fmt.Println(b.typ)
-	fmt.Println(reflect.Kind(b.typ.kind&((1 << 5) - 1)))
+	fmt.Println(b.Typ)
+	fmt.Println(reflect.Kind(b.Typ.Kind&((1 << 5) - 1)))
 }
+
